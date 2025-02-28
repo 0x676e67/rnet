65a150e96e74e5ffaaf089b32708d7b9ff6f4a07
0x676e67 <gngppz@gmail.com>
2025-03-01 07:55:30 +0800

    perf(stream): Release the GIL while the Stream is waiting for the future

--- src/stream.rs
+++ src/stream.rs
@@ -11,7 +11,7 @@ pub struct SyncStream {
 }
 
 pub struct AsyncStream {
-    stream: Pin<Box<dyn futures_util::Stream<Item = PyObject> + Send + Sync + 'static>>,
+    stream: Pin<Box<dyn Stream<Item = PyObject> + Send + Sync + 'static>>,
 }
 
 impl SyncStream {
@@ -23,7 +23,7 @@ impl SyncStream {
 
 impl AsyncStream {
     #[inline]
-    pub fn new(stream: impl futures_util::Stream<Item = PyObject> + Send + Sync + 'static) -> Self {
+    pub fn new(stream: impl Stream<Item = PyObject> + Send + Sync + 'static) -> Self {
         AsyncStream {
             stream: Box::pin(stream),
         }
@@ -55,13 +55,13 @@ impl Stream for AsyncStream {
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Option<Self::Item>> {
         let waker = cx.waker();
-        Python::with_gil(|gil| {
-            gil.allow_threads(|| {
+        Python::with_gil(|py| {
+            py.allow_threads(|| {
                 self.stream
                     .as_mut()
                     .poll_next(&mut Context::from_waker(waker))
             })
-            .map(|item| item.map(|item| Python::with_gil(|py| downcast_bound_bytes(py, item))))
+            .map(|item| item.map(|item| downcast_bound_bytes(py, item)))
         })
     }
 }
