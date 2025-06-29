8d981fa75c197baf3432ab031d7a0c7ec34016f8
0x676e67 <gngppz@gmail.com>
2025-06-29 18:19:58 +0800

    update

--- python/rnet/__init__.pyi
+++ python/rnet/__init__.pyi
@@ -10,16 +10,11 @@ from typing import (
     TypedDict,
 )
 from pathlib import Path
+from enum import Enum, auto
 
 from .impersonate import ImpersonateOption, Impersonate
 from .header import HeaderMap
 from .cookie import Cookie
-from .enums import (
-    LookupIpStrategy,
-    Method,
-    TlsVersion,
-    Version,
-)
 
 try:
     from typing import Unpack, NotRequired
@@ -1235,3 +1230,49 @@ class WebSocket:
         * `code` - An optional close code.
         * `reason` - An optional reason for closing.
         """
+
+class LookupIpStrategy(Enum):
+    r"""
+    The lookup ip strategy.
+    """
+
+    Ipv4Only = auto()
+    Ipv6Only = auto()
+    Ipv4AndIpv6 = auto()
+    Ipv6thenIpv4 = auto()
+    Ipv4thenIpv6 = auto()
+
+class Method(Enum):
+    r"""
+    An HTTP method.
+    """
+
+    GET = auto()
+    HEAD = auto()
+    POST = auto()
+    PUT = auto()
+    DELETE = auto()
+    OPTIONS = auto()
+    TRACE = auto()
+    PATCH = auto()
+
+class TlsVersion(Enum):
+    r"""
+    The TLS version.
+    """
+
+    TLS_1_0 = auto()
+    TLS_1_1 = auto()
+    TLS_1_2 = auto()
+    TLS_1_3 = auto()
+
+class Version(Enum):
+    r"""
+    An HTTP version.
+    """
+
+    HTTP_09 = auto()
+    HTTP_10 = auto()
+    HTTP_11 = auto()
+    HTTP_2 = auto()
+    HTTP_3 = auto()
