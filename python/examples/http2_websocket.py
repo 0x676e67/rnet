529a83d7a49dd893fd33986b1ff1794b02c75c27
0x676e67 <gngppz@gmail.com>
2025-09-01 06:28:44 +0800

    refactor(ws): drop iterator impl (#317)

--- python/examples/http2_websocket.py
+++ python/examples/http2_websocket.py
@@ -1,22 +1,34 @@
 import asyncio
+import datetime
 import signal
 import rnet
 from rnet import Message, WebSocket
+from rnet import exceptions
 
 
 async def send_message(ws):
+    print("Starting to send messages...")
     for i in range(20):
         print(f"Sending: Message {i + 1}")
         await ws.send(Message.from_text(f"Message {i + 1}"))
         await asyncio.sleep(0.1)
 
 
-async def receive_message(ws):
-    async for message in ws:
-        print("Received: ", message)
-        if message.data == b"Message 20":
-            print("Closing connection...")
-            break
+async def recv_message(ws):
+    print("Starting to receive messages...")
+    while True:
+        try:
+            message = await ws.recv(timeout=datetime.timedelta(milliseconds=10))
+            print("Received: ", message)
+            if message is None:
+                print("Connection closed by server.")
+                break
+
+            if message.data == b"Message 20":
+                print("Closing connection...")
+                break
+        except exceptions.TimeoutError:
+            continue
 
 
 """
@@ -43,12 +55,10 @@ async def main():
         if ws.status.as_int() == 200:
             print("WebSocket connection established successfully.")
             send_task = asyncio.create_task(send_message(ws))
-            receive_task = asyncio.create_task(receive_message(ws))
+            receive_task = asyncio.create_task(recv_message(ws))
 
             async def close():
                 await ws.close()
-                send_task.cancel()
-                receive_task.cancel()
 
             loop = asyncio.get_running_loop()
             for sig in (signal.SIGINT, signal.SIGTERM):
